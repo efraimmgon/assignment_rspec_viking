15a203b5c66908f13665fd594cd7cef56b46d4e8
efraimmgon <efraimmaugusto@gmail.com>
2017-02-20 11:33:22 -0400

    bow_spec complete

--- spec/bow_spec.rb
+++ spec/bow_spec.rb
@@ -1,13 +1,23 @@
 require "weapons/bow.rb"
 
 describe Bow do
-  it "can count its arrows"
+  let(:bow) { Bow.new }
 
-  it "starts with 10 arrows by default"
+  it "starts with 10 arrows by default" do
+    expect(bow.instance_variable_get(:@arrows)).to eq(10)
+  end
 
-  it "starts with the specified number of arrows when created"
+  it "starts with the specified number of arrows when created" do
+    expect(Bow.new(20).instance_variable_get(:@arrows)).to eq(20)
+  end
 
-  it "reduces arrows by 1 when used"
+  it "reduces arrows by 1 when used" do
+    bow.use
+    expect(bow.instance_variable_get(:@arrows)).to eq(9)
+  end
 
-  it "throws an error if used with no arrows left"
+  it "throws an error if used with no arrows left" do
+    empty_bow = Bow.new(0)
+    expect{ empty_bow.use }.to raise_error("Out of arrows")
+  end
 end
